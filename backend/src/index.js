aeaa5ec6def60080e7011b9be7b8657861569ad6
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2024-11-21 01:04:57 +0530

    Created signup api

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js"
+import dotenv from 'dotenv'
+import { connectDB } from "./lib/db.js";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+app.use(bodyParser.json());
+
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, ()=>{
+    console.log("Server running on port 5001"); 
+    connectDB();
+})
