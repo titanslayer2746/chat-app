bbe7ed824b8a1a316b4a081af6339e15768011bb
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2024-12-21 13:14:26 +0530

    adding protectRoute middleware and updateProfile API plus minor bug fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,14 @@ import authRoutes from "./routes/auth.route.js"
 import dotenv from 'dotenv'
 import { connectDB } from "./lib/db.js";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes)
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -7,4 +8,6 @@ router.post('/signup', signup)
 router.post('/login', login)
 router.post('/logout', logout)
 
+router.put('/update-profile',protectRoute, updateProfile)
+router.get('/check',protectRoute,checkAuth)
 export default router;
