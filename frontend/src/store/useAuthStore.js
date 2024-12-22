24fd0550cf015ccbe2936b9b5754bc26c619106f
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2024-12-22 10:59:56 +0530

    Sidebar component added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -8,6 +8,8 @@ export const useAuthStore = create((set)=>({
     isLoggingIn: false,
     isUpdatingProfile: false,
     isCheckingAuth: true,
+    onlineUsers: [],
+    
     checkAuth: async()=>{
         try {
             const res = await axiosInstance.get("/auth/check")
