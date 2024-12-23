cd8e7629687e9411e88fe470c01bf748f44cb59f
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2024-12-23 11:49:14 +0530

    Updated Configuration for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,14 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import { app,server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 
 const PORT = process.env.PORT
+const __dirname = path.resolve()
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
@@ -23,6 +26,15 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+      });
+}
 
 server.listen(PORT, ()=>{
     console.log("Server running on port 5001"); 
